6b6acebf10652b438975bdd54ffd7f5e103d45c1
differentstuff <83787027+differentstuff@users.noreply.github.com>
2023-05-03 21:24:20 +0200

    Rename AD Sync Cycle Delta over Remote.ps1 to Powershell/AD Sync Cycle Delta over Remote.ps1

--- AD Sync Cycle Delta over Remote.ps1
+++
@@ -1,4 +0,0 @@
-﻿$creds = "admin@domain.com"
-$targetIP = "10.50.0.10"
-
-Invoke-Command -ComputerName $targetIP -ScriptBlock { start-adsyncsynccycle -policy delta } -credential $creds
---
+++ Powershell/AD Sync Cycle Delta over Remote.ps1
@@ -0,0 +1,4 @@
+﻿$creds = "admin@domain.com"
+$targetIP = "10.50.0.10"
+
+Invoke-Command -ComputerName $targetIP -ScriptBlock { start-adsyncsynccycle -policy delta } -credential $creds
